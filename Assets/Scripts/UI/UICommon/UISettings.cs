1e2734283cfc3eef6ffcc5bbac17793fb91dcf30
LiztyStalker <31952112+LiztyStalker@users.noreply.github.com>
2021-12-10 18:00:17 +0900

    NEW : UISettings 초안 개발 및 단위테스트

---
+++ Assets/Scripts/UI/UICommon/UISettings.cs
@@ -0,0 +1,309 @@
+namespace SEF.UI.Toolkit
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UIElements;
+    using Storage;
+
+    [RequireComponent(typeof(UIDocument))]
+    public class UISettings : MonoBehaviour
+    {
+
+        private readonly string SETTINGS_BGM_KEY = "BGM_VALUE";
+        private readonly string SETTINGS_SFX_KEY = "SFX_VALUE";
+        private readonly string SETTINGS_FRAME_KEY = "FRAME_VALUE";
+        private readonly string SETTINGS_LANGUAGE_KEY = "LANGUAGE";
+        private readonly string SETTINGS_HIT_KEY = "ISHIT";
+        private readonly string SETTINGS_EFFECT_KEY = "ISEFFECT";
+
+
+        private readonly string PATH_DEFAULT_SETTING = "Assets/UIToolkit/DefaultPanelSettings.asset";
+        private readonly string PATH_UI_SETTINGS_UXML = "Assets/Scripts/UI/UICommon/UISettingsUXML.uxml";
+
+        private VisualElement _root = null;
+
+        private Label _versionLabel;
+
+        private Slider _bgmSlider;
+        private Label _bgmVolumeLabel;
+
+        private Slider _sfxSlider;
+        private Label _sfxVolumeLabel;
+
+        private Slider _frameSlider;
+        private Label _frameValueLabel;
+
+        private Button _langLeftButton;
+        private Button _langRightButton;
+        private Label _langLabel;
+
+        private Toggle _uiHitActivateToggle;
+        private Toggle _effectActivateToggle;
+
+        private Button _saveButton;
+        private Button _loadButton;
+
+        private Button _gpgsButton;
+        private Button _qnaButton;
+
+        private Button _exitButton;
+
+        public static UISettings Create()
+        {
+            var obj = new GameObject();
+            obj.name = "UI@Settings";
+            return obj.AddComponent<UISettings>();
+        }
+
+        public void Initialize()
+        {
+            if (_root == null)
+            {
+#if UNITY_EDITOR || UNITY_INCLUDE_TESTS
+                var _uiDocument = GetComponent<UIDocument>();
+                _uiDocument.panelSettings = DataStorage.LoadAssetAtPath<PanelSettings>(PATH_DEFAULT_SETTING);
+
+                var asset = DataStorage.LoadAssetAtPath<VisualTreeAsset>(PATH_UI_SETTINGS_UXML);
+                _uiDocument.visualTreeAsset = asset;
+                _root = _uiDocument.rootVisualElement;
+                Debug.Assert(_root != null, $"{PATH_UI_SETTINGS_UXML} UI를 구성하지 못했습니다");
+#else
+        
+#endif
+            }
+
+
+
+
+            _versionLabel = _root.Q<Label>("version_label");
+
+            _bgmSlider = _root.Q<Slider>("bgm_slider");
+            _bgmVolumeLabel = _root.Q<Label>("bgm_slider_value_label");
+
+            _sfxSlider = _root.Q<Slider>("sfx_slider");
+            _sfxVolumeLabel = _root.Q<Label>("sfx_slider_value_label");
+
+            _frameSlider = _root.Q<Slider>("frame_slider");
+            _frameValueLabel = _root.Q<Label>("frame_slider_value_label");
+
+            _langLeftButton = _root.Q<Button>("lang_left_button");
+            _langRightButton = _root.Q<Button>("lang_right_button");
+            _langLabel = _root.Q<Label>("lang_value_label");
+
+            _uiHitActivateToggle = _root.Q<Toggle>("hit_toggle");
+            _effectActivateToggle = _root.Q<Toggle>("effect_toggle");
+
+            _saveButton = _root.Q<Button>("save_button");
+            _loadButton = _root.Q<Button>("load_button");
+
+            _gpgsButton = _root.Q<Button>("gpgs_button");
+            _qnaButton = _root.Q<Button>("qna_button");
+
+            _exitButton = _root.Q<Button>("exit_button");
+
+
+            Debug.Assert(_versionLabel != null, $"{PATH_UI_SETTINGS_UXML} version_label 을 구성하지 못했습니다");
+            Debug.Assert(_bgmSlider != null, $"{PATH_UI_SETTINGS_UXML} bgm_slider 을 구성하지 못했습니다");
+            Debug.Assert(_bgmVolumeLabel != null, $"{PATH_UI_SETTINGS_UXML} bgm_slider_value_label 을 구성하지 못했습니다");
+            Debug.Assert(_sfxSlider != null, $"{PATH_UI_SETTINGS_UXML} sfx_slider 을 구성하지 못했습니다");
+            Debug.Assert(_sfxVolumeLabel != null, $"{PATH_UI_SETTINGS_UXML} sfx_slider_value_label 을 구성하지 못했습니다");
+            Debug.Assert(_frameSlider != null, $"{PATH_UI_SETTINGS_UXML} frame_slider 을 구성하지 못했습니다");
+            Debug.Assert(_frameValueLabel != null, $"{PATH_UI_SETTINGS_UXML} frame_slider_value_label 을 구성하지 못했습니다");
+            Debug.Assert(_langLeftButton != null, $"{PATH_UI_SETTINGS_UXML} lang_left_button 을 구성하지 못했습니다");
+            Debug.Assert(_langRightButton != null, $"{PATH_UI_SETTINGS_UXML} lang_right_button 을 구성하지 못했습니다");
+            Debug.Assert(_langLabel != null, $"{PATH_UI_SETTINGS_UXML} lang_value_label 을 구성하지 못했습니다");
+            Debug.Assert(_uiHitActivateToggle != null, $"{PATH_UI_SETTINGS_UXML} hit_toggle 을 구성하지 못했습니다");
+            Debug.Assert(_effectActivateToggle != null, $"{PATH_UI_SETTINGS_UXML} effect_toggle 을 구성하지 못했습니다");
+            Debug.Assert(_saveButton != null, $"{PATH_UI_SETTINGS_UXML} save_button 을 구성하지 못했습니다");
+            Debug.Assert(_loadButton != null, $"{PATH_UI_SETTINGS_UXML} load_button 을 구성하지 못했습니다");
+            Debug.Assert(_gpgsButton != null, $"{PATH_UI_SETTINGS_UXML} gpgs_button 을 구성하지 못했습니다");
+            Debug.Assert(_qnaButton != null, $"{PATH_UI_SETTINGS_UXML} qna_button 을 구성하지 못했습니다");
+            Debug.Assert(_exitButton != null, $"{PATH_UI_SETTINGS_UXML} exit_button 을 구성하지 못했습니다");
+
+            Load();
+
+
+
+            _versionLabel.text = Application.version;
+
+            RegisterEvents();
+
+            Hide();
+
+        }
+
+        public void CleanUp()
+        {
+            _closedEvent = null;
+
+            UnRegisterEvents();
+
+            _exitButton = null;
+            _root = null;
+        }
+
+        private void RegisterEvents()
+        {
+            _bgmSlider.RegisterCallback<ChangeEvent<float>>(e => OnBGMEvent(e.newValue));
+            _sfxSlider.RegisterCallback<ChangeEvent<float>>(e => OnSFXEvent(e.newValue));
+            _frameSlider.RegisterCallback<ChangeEvent<float>>(e => OnFrameEvent(e.newValue));
+
+            _langLeftButton.RegisterCallback<ClickEvent>(e => OnLeftLanguageEvent());
+            _langRightButton.RegisterCallback<ClickEvent>(e => OnRightLanguageEvent());
+
+            _uiHitActivateToggle.RegisterCallback<ChangeEvent<bool>>(e => OnHitToggleEvent(e.newValue));
+            _effectActivateToggle.RegisterCallback<ChangeEvent<bool>>(e => OnEffectToggleEvent(e.newValue));
+
+            _saveButton.RegisterCallback<ClickEvent>(e => OnSaveEvent());
+            _loadButton.RegisterCallback<ClickEvent>(e => OnLoadEvent());
+            _gpgsButton.RegisterCallback<ClickEvent>(e => OnGPGSEvent());
+            _qnaButton.RegisterCallback<ClickEvent>(e => OnQnAEvent());
+
+            _exitButton.RegisterCallback<ClickEvent>(e => OnExitEvent());
+        }
+
+        private void UnRegisterEvents()
+        {
+            _bgmSlider.UnregisterCallback<ChangeEvent<float>>(e => OnBGMEvent(e.newValue));
+            _sfxSlider.UnregisterCallback<ChangeEvent<float>>(e => OnSFXEvent(e.newValue));
+            _frameSlider.UnregisterCallback<ChangeEvent<float>>(e => OnFrameEvent(e.newValue));
+
+            _langLeftButton.UnregisterCallback<ClickEvent>(e => OnLeftLanguageEvent());
+            _langRightButton.UnregisterCallback<ClickEvent>(e => OnRightLanguageEvent());
+
+            _uiHitActivateToggle.UnregisterCallback<ChangeEvent<bool>>(e => OnHitToggleEvent(e.newValue));
+            _effectActivateToggle.UnregisterCallback<ChangeEvent<bool>>(e => OnEffectToggleEvent(e.newValue));
+
+            _saveButton.UnregisterCallback<ClickEvent>(e => OnSaveEvent());
+            _loadButton.UnregisterCallback<ClickEvent>(e => OnLoadEvent());
+            _gpgsButton.UnregisterCallback<ClickEvent>(e => OnGPGSEvent());
+            _qnaButton.UnregisterCallback<ClickEvent>(e => OnQnAEvent());
+
+            _exitButton.UnregisterCallback<ClickEvent>(e => OnExitEvent());
+        }
+
+        private void OnBGMEvent(float value)
+        {
+            _bgmVolumeLabel.text = value.ToString();
+        }
+
+        private void OnSFXEvent(float value)
+        {
+            _sfxVolumeLabel.text = value.ToString();
+        }
+
+        private void OnFrameEvent(float value)
+        {
+            _frameValueLabel.text = value.ToString();
+        }
+
+        private void OnLeftLanguageEvent() 
+        {
+            _langLabel.text = "";
+            Debug.Log("Language Left Button");
+        }
+        private void OnRightLanguageEvent() 
+        {
+            _langLabel.text = "";
+            Debug.Log("Language Right Button");
+        }
+
+        private void OnHitToggleEvent(bool isOn) 
+        {
+            Debug.Log($"OnHitToggleEvent {isOn}");
+        }
+        private void OnEffectToggleEvent(bool isOn) 
+        {
+            Debug.Log($"OnEffectToggleEvent {isOn}");
+        }
+
+        private void OnSaveEvent() 
+        {
+            Debug.Log("OnSaveEvent");
+        }
+
+        private void OnLoadEvent()
+        {
+            Debug.Log("OnLoadEvent");
+        }
+
+        private void OnGPGSEvent() 
+        {
+            Debug.Log("OnGPGSEvent");
+        }
+
+        private void OnQnAEvent() 
+        {
+            Debug.Log("OnQnAEvent");
+        }
+
+
+        public void Show(System.Action closedCallback = null)
+        {
+            _root.style.display = DisplayStyle.Flex;
+            _closedEvent = closedCallback;
+        }
+
+
+        public void Hide()
+        {
+            _root.style.display = DisplayStyle.None;
+
+            OnClosedEvent();
+            Save();
+
+            _closedEvent = null;
+        }
+
+
+
+
+
+        #region ##### Event #####
+
+
+        private System.Action _closedEvent;
+
+
+        private void OnExitEvent()
+        {
+            Hide();
+        }
+
+        private void OnClosedEvent()
+        {
+            _closedEvent?.Invoke();
+        }
+
+        #endregion
+
+
+        private void Load()
+        {
+            _bgmSlider.value = (float)PlayerPrefs.GetInt(SETTINGS_BGM_KEY, 100);
+            _bgmVolumeLabel.text = _bgmSlider.value.ToString();
+
+            _sfxSlider.value = (float)PlayerPrefs.GetInt(SETTINGS_SFX_KEY, 100);
+            _sfxVolumeLabel.text = _sfxSlider.value.ToString();
+
+            _frameSlider.value = (float)PlayerPrefs.GetInt(SETTINGS_FRAME_KEY, 30);
+            _frameValueLabel.text = _frameSlider.value.ToString();
+
+            _langLabel.text = PlayerPrefs.GetString(SETTINGS_LANGUAGE_KEY, "Korean");
+
+            _uiHitActivateToggle.value = (PlayerPrefs.GetInt(SETTINGS_HIT_KEY, 1) == 1) ? true : false;
+            _effectActivateToggle.value = (PlayerPrefs.GetInt(SETTINGS_EFFECT_KEY, 1) == 1) ? true : false;
+        }
+
+        private void Save()
+        {
+            PlayerPrefs.SetInt(SETTINGS_BGM_KEY, (int)_bgmSlider.value);
+            PlayerPrefs.SetInt(SETTINGS_SFX_KEY, (int)_sfxSlider.value);
+            PlayerPrefs.SetInt(SETTINGS_FRAME_KEY, (int)_frameSlider.value);
+            PlayerPrefs.SetString(SETTINGS_LANGUAGE_KEY, _langLabel.text);
+            PlayerPrefs.SetInt(SETTINGS_HIT_KEY, (_uiHitActivateToggle.value) ? 1 : 0);
+            PlayerPrefs.SetInt(SETTINGS_EFFECT_KEY, (_effectActivateToggle.value) ? 1 : 0);
+        }
+    }
+}
--- Assets/Tests/PlayMode/UICommonTest.cs
+++ Assets/Tests/PlayMode/UICommonTest.cs
@@ -8,7 +8,6 @@ using SEF.UI.Toolkit;
 public class UICommonTest
 {
     private Camera _camera;
-    private UIPopup _uiPopup;
 
 
     private void CreateCamera()
@@ -25,27 +24,49 @@ public class UICommonTest
         Object.DestroyImmediate(_camera.gameObject);
     }
 
+
+    private UIPopup CreateUIPopup()
+    {
+        var _uiPopup = UIPopup.Create();
+        _uiPopup.Initialize();
+        return _uiPopup;
+    }
+
+    private void DestoryUIPopup(UIPopup uiPopup)
+    {
+        uiPopup.CleanUp();
+    }
+
+    private UISettings CreateUISettings()
+    {
+        var _uiSettings = UISettings.Create();
+        _uiSettings.Initialize();
+        return _uiSettings;
+    }
+
+    private void DestoryUISettings(UISettings uiSettings)
+    {
+        uiSettings.CleanUp();
+    }
+
     [SetUp]
     public void SetUp()
     {
         CreateCamera();
-        _uiPopup = UIPopup.Create();
-        _uiPopup.Initialize();
     }
 
     [TearDown]
     public void TearDown()
     {
         DestoryCamera();
-        _uiPopup.CleanUp();
-        _uiPopup = null;
     }
 
     [UnityTest]
     public IEnumerator UICommonTest_UIPopup_Normal()
     {
         bool isClicked = false;
-        _uiPopup.ShowPopup("UIPopup Normal Test", delegate
+        var uiPopup = CreateUIPopup();
+        uiPopup.ShowPopup("UIPopup Normal Test", delegate
         {
             Debug.Log("Normal ClosedEvent Callback");
             isClicked = true;
@@ -54,13 +75,15 @@ public class UICommonTest
         while (!isClicked) {
             yield return null;
         }
+        DestoryUIPopup(uiPopup);
     }
 
     [UnityTest]
     public IEnumerator UICommonTest_UIPopup_Apply()
     {
         bool isClicked = false;
-        _uiPopup.ShowPopup("UIPopup Apply Test", "확인", delegate
+        var uiPopup = CreateUIPopup();
+        uiPopup.ShowPopup("UIPopup Apply Test", "확인", delegate
         {
             Debug.Log("Apply Callback");
             isClicked = true;
@@ -69,13 +92,15 @@ public class UICommonTest
         {
             yield return null;
         }
+        DestoryUIPopup(uiPopup);
     }
 
     [UnityTest]
     public IEnumerator UICommonTest_UIPopup_ApplyCancel()
     {
         bool isClicked = false;
-        _uiPopup.ShowPopup("UIPopup ApplyCancel Test", "확인", "취소", delegate {
+        var uiPopup = CreateUIPopup();
+        uiPopup.ShowPopup("UIPopup ApplyCancel Test", "확인", "취소", delegate {
             Debug.Log("Apply Callback");
             isClicked = true;
         }, delegate {
@@ -86,7 +111,25 @@ public class UICommonTest
         {
             yield return null;
         }
+        DestoryUIPopup(uiPopup);
     }
 
+    [UnityTest]
+    public IEnumerator UICommonTest_UISettings_Show()
+    {
+        bool isClicked = false;
+        var uiSettings = CreateUISettings();
+        uiSettings.Initialize();
+        uiSettings.Show(delegate {
+            Debug.Log("Apply Callback");
+            isClicked = true;
+        });
+        while (!isClicked)
+        {
+            yield return null;
+        }
+        DestoryUISettings(uiSettings);
+
+    }
 
 }
