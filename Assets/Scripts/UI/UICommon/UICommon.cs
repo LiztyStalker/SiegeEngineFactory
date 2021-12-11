321e045a182b47472da146b117985f7d60a7f801
LiztyStalker <31952112+LiztyStalker@users.noreply.github.com>
2021-12-11 14:36:51 +0900

    NEW : UICommon 적용

--- Assets/Scripts/UI/UICommon/UICommon.cs
+++ Assets/Scripts/UI/UICommon/UICommon.cs
@@ -7,33 +7,107 @@ namespace SEF.UI
 
     public class UICommon : MonoBehaviour
     {
+        private static UICommon _current;
+
+
+        public static UICommon Current
+        {
+            get
+            {
+                if(_current == null)
+                {
+                    _current = Create();
+                    _current.Initialize();
+                }
+                return _current;
+            }
+        }
 
         private UIPopup _uiPopup;
+        private UICredits _uiCredits;
+        private UISettings _uiSettings;
 
+        private static UICommon Create()
+        {
+            var obj = new GameObject();
+            obj.name = "UI@Common";
+            return obj.AddComponent<UICommon>();
+        }
 
         public void Initialize()
         {
+
             _uiPopup = GetComponentInChildren<UIPopup>(true);
-            _uiPopup.Initialize();
-        }
 
-        private void Awake()
-        {
-            Initialize();
-        }
-
-        private void Start()
-        {
-            _uiPopup.ShowPopup("UIPopup Apply Test", "»Æ¿Œ", delegate
+            if (_uiPopup == null)
             {
-                Debug.Log("Apply Callback");
-            });
+                _uiPopup = UIPopup.Create();
+                _uiPopup.transform.SetParent(transform);
+            }
+
+            _uiPopup.Initialize();
+
+
+
+            _uiCredits = GetComponentInChildren<UICredits>(true);
+
+            if (_uiCredits == null)
+            {
+                _uiCredits = UICredits.Create();
+                _uiCredits.transform.SetParent(transform);
+            }
+            _uiCredits.Initialize();
+
+
+            _uiSettings = GetComponentInChildren<UISettings>(true);
+
+            if (_uiSettings == null)
+            {
+                _uiSettings = UISettings.Create();
+                _uiSettings.transform.SetParent(transform);
+            }
+            _uiSettings.Initialize();            
         }
+
 
         public void CleanUp()
         {
             _uiPopup.CleanUp();
+            _uiSettings.CleanUp();
+            _uiCredits.CleanUp();
+            _current = null;
         }
+
+
+
+
+
+        public void ShowPopup(string msg, System.Action closedCallback = null)
+        {
+            _uiPopup.ShowPopup(msg, closedCallback);
+        }
+
+
+        public void ShowPopup(string msg, string applyText, System.Action applyCallback = null, System.Action closedCallback = null)
+        {
+            _uiPopup.ShowPopup(msg, applyText, applyCallback, closedCallback);
+        }
+
+        public void ShowPopup(string msg, string applyText, string cancelText, System.Action applyCallback = null, System.Action cancelCallback = null, System.Action closedCallback = null)
+        {
+            _uiPopup.ShowPopup(msg, applyText, cancelText, applyCallback, cancelCallback, closedCallback);
+        }
+
+        public void ShowSettings(System.Action closedCallback = null)
+        {
+            _uiSettings.Show(closedCallback);
+        }
+
+        public void ShowCredits(System.Action closedCallback = null)
+        {
+            _uiCredits.Show(closedCallback);
+        }
+
 
     }
 }
