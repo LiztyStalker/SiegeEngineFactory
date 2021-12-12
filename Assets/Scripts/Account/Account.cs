62a6c4b9ee807bbcf094f77ca72de843604e11a3
LiztyStalker <31952112+LiztyStalker@users.noreply.github.com>
2021-12-13 01:14:47 +0900

    NEW : Account -> AccountIO 분할

--- Assets/Scripts/Account/Account.cs
+++ Assets/Scripts/Account/Account.cs
@@ -40,44 +40,4 @@ namespace SEF.Account
         }
     }
 
-    internal class AccountIO
-    {
-
-#if UNITY_EDITOR || UNITY_INCLUDE_TESTS
-
-        public static void Load(System.Action<float> loadCallback, System.Action<TYPE_IO_RESULT> endCallback) 
-        {
-            var loader = DataLoader.Create();
-            loader.LoadTest(loadCallback, result =>
-            {
-                endCallback?.Invoke(result);
-                loader.Dispose();
-            });
-        }
-
-        public static void Save(System.Action saveCallback, System.Action endCallback) 
-        {
-            saveCallback?.Invoke();
-            endCallback?.Invoke();
-        }
-#else
-
-        public static void Load(System.Action<float> loadCallback, System.Action<TYPE_IO_RESULT> endCallback) 
-        {
-            var loader = DataLoader.Create();
-            loader.Load(loadCallback, result =>
-            {
-                endCallback?.Invoke(result);
-                loader.Dispose();
-            });
-        }
-
-        public static void Save(System.Action saveCallback, System.Action endCallback) 
-        {
-            saveCallback?.Invoke();
-            endCallback?.Invoke();
-        }
-
-#endif
-    }
 }
