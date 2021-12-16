33fb3b7d31c068ad6ef1342c599333854ae372a4
LiztyStalker <31952112+LiztyStalker@users.noreply.github.com>
2021-12-16 16:46:01 +0900

    FIX : BulletActor Data UtilityManager 네임스페이스 적용

--- Assets/Scripts/Utility/BulletManager/BulletActor.cs
+++ Assets/Scripts/Utility/BulletManager/BulletActor.cs
@@ -1,233 +1,236 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using PoolSystem;
-
-public class BulletActor : MonoBehaviour, IPoolElement
+namespace UtilityManager
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using PoolSystem;
 
-    private const float ARRIVE_DISTANCE = 0.1f;
-
-    private GameObject _prefab;
-    private BulletData _data;
-
-    private System.Action<BulletActor> _arrivedEvent;
-    private System.Action<BulletActor> _inactivateEvent;
-
-    private Vector2 _startPos;
-    private Vector2 _arrivePos;
-
-    private float _nowTime = 0f;
-
-    private SpriteRenderer _spriteRenderer {get; set;}
-    private ParticleSystem[] _particles { get; set; }
-
-    private bool _isEffectActivate = false;
-
-    private void SetName()
+    public class BulletActor : MonoBehaviour, IPoolElement
     {
-        gameObject.name = $"BulletActor_{_data.name}";
-    }
 
-    public void SetData(BulletData data)
-    {
-        _data = data;
-        SetName();
-    }
+        private const float ARRIVE_DISTANCE = 0.1f;
 
-    public void SetPosition(Vector2 startPos, Vector2 arrivePos)
-    {
-        _startPos = startPos;
-        _arrivePos = arrivePos;
-    }
+        private GameObject _prefab;
+        private BulletData _data;
 
-    public void SetOnArrivedListener(System.Action<BulletActor> act) => _arrivedEvent = act;
-    public void SetOnInactivateListener(System.Action<BulletActor> act) => _inactivateEvent = act;
+        private System.Action<BulletActor> _arrivedEvent;
+        private System.Action<BulletActor> _inactivateEvent;
 
-    public bool Contains(BulletData data) => _data == data;
+        private Vector2 _startPos;
+        private Vector2 _arrivePos;
 
-    public void Activate()
-    {
-        _nowTime = 0f;
-        _isEffectActivate = false;
+        private float _nowTime = 0f;
 
-        if (_prefab == null)
+        private SpriteRenderer _spriteRenderer { get; set; }
+        private ParticleSystem[] _particles { get; set; }
+
+        private bool _isEffectActivate = false;
+
+        private void SetName()
         {
-            //flyweight 필요
-            _prefab = Instantiate(_data.prefab);
-            _prefab.transform.SetParent(transform);
-            _prefab.transform.localPosition = Vector3.zero;
+            gameObject.name = $"BulletActor_{_data.name}";
         }
 
-        transform.position = _startPos;
-
-        _spriteRenderer = GetComponentInChildren<SpriteRenderer>(true);
-        if (_spriteRenderer != null)
+        public void SetData(BulletData data)
         {
-            //_spriteRenderer.sortingLayerName = "FrontEffect";
-            //_spriteRenderer.sortingOrder = (int)-transform.position.y - 5;
-            _spriteRenderer.enabled = true;
+            _data = data;
+            SetName();
         }
 
-        _particles = _prefab.GetComponentsInChildren<ParticleSystem>(true);
-        //if (_particles != null)
-        //{
-        //    for (int i = 0; i < _particles.Length; i++)
-        //    {
-        //        var psRenderer = _particles[i].GetComponent<ParticleSystemRenderer>();
-        //        if(psRenderer != null)
-        //        {
-        //            psRenderer.sortingLayerName = "FrontEffect";
-        //            psRenderer.sortingOrder = (int)-transform.position.y + 10;
-        //        }
-        //    }
-        //}
-        gameObject.SetActive(true);
-
-    }
-
-    public void Inactivate()
-    {
-        gameObject.SetActive(false);
-        _startPos = Vector2.zero;
-        _arrivePos = Vector2.zero;
-        _nowTime = 0f;
-        _inactivateEvent?.Invoke(this);
-    }
-
-    public void CleanUp()
-    {
-        DestroyImmediate(_prefab);
-        _data = null;
-        _arrivedEvent = null;
-        _inactivateEvent = null;
-        _startPos = Vector2.zero;
-        _arrivePos = Vector2.zero;
-        _prefab = null;
-    }
-
-    private void Update()
-    {
-
-        _nowTime = CalculateTime(_nowTime);
-        transform.position = GetPosition(_startPos, _arrivePos, _nowTime);
-    
-        if(_data.IsRotate) transform.eulerAngles = GetEuler(_startPos, _arrivePos, _nowTime);
-
-        //Debug.Log(transform.position + " " + _startPos + " " + _arrivePos + " " + _nowTime);
-
-        if (Vector2.Distance(transform.position, _arrivePos) < ARRIVE_DISTANCE)
+        public void SetPosition(Vector2 startPos, Vector2 arrivePos)
         {
-            if (!_isEffectActivate)
+            _startPos = startPos;
+            _arrivePos = arrivePos;
+        }
+
+        public void SetOnArrivedListener(System.Action<BulletActor> act) => _arrivedEvent = act;
+        public void SetOnInactivateListener(System.Action<BulletActor> act) => _inactivateEvent = act;
+
+        public bool Contains(BulletData data) => _data == data;
+
+        public void Activate()
+        {
+            _nowTime = 0f;
+            _isEffectActivate = false;
+
+            if (_prefab == null)
             {
-                OnArriveEvent();
-                EffectManager.Activate(_data.ArriveEffectData, transform.position);
-                _isEffectActivate = true;
+                //flyweight 필요
+                _prefab = Instantiate(_data.prefab);
+                _prefab.transform.SetParent(transform);
+                _prefab.transform.localPosition = Vector3.zero;
             }
-        }
 
-        if (_isEffectActivate)
-        {
-            ReadyInactivate();
-        }
+            transform.position = _startPos;
 
-    }
-
-    private float CalculateTime(float nowTime)
-    {
-        return nowTime += Time.deltaTime * _data.MovementSpeed;
-    }
-
-    private Vector3 GetEuler(Vector3 startPos, Vector3 arrivePos, float nowTime)
-    {
-        switch (_data.TypeBulletAction)
-        {
-            case BulletData.TYPE_BULLET_ACTION.Drop:
-                startPos = new Vector2(arrivePos.x, arrivePos.y + 10f);
-                break;              
-            //case BulletData.TYPE_BULLET_ACTION.Curve:
-            //    //현재 위치에서 미래 위치를 예측해서 각도 찾기
-            //    var futurePos = GetSlerp(startPos, arrivePos, CalculateTime(nowTime));
-            //    startPos = transform.position;
-            //    arrivePos = futurePos;
-            //    break;
-        }
-
-        var direction = arrivePos - startPos;
-        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        return Vector3.forward * angle;
-    }
-
-    private Vector2 GetPosition(Vector3 startPos, Vector3 arrivePos, float nowTime)
-    {
-        switch (_data.TypeBulletAction)
-        {
-            case BulletData.TYPE_BULLET_ACTION.Curve:
-            case BulletData.TYPE_BULLET_ACTION.Move:
-                return Vector2.MoveTowards(startPos, arrivePos, nowTime);
-            case BulletData.TYPE_BULLET_ACTION.Drop:
-                var pos = new Vector2(arrivePos.x, arrivePos.y + 10f);
-                return Vector2.MoveTowards(pos, arrivePos, nowTime);
-            case BulletData.TYPE_BULLET_ACTION.Direct:
-                return arrivePos;
-            //case BulletData.TYPE_BULLET_ACTION.Curve:
-                //return GetSlerp(startPos, arrivePos, nowTime);
-
-        }
-        return Vector2.zero;
-    }
-
-    private Vector2 GetSlerp(Vector3 startPos, Vector3 arrivePos, float nowTime)
-    {
-        var center = (startPos + arrivePos) * 8f;
-        center -= Vector3.up;
-
-        var startRelPos = startPos - center;
-        var arriveRelPos = arrivePos - center;
-        var nowPos = Vector3.Slerp(startRelPos, arriveRelPos, nowTime);
-        nowPos += center;
-        return nowPos;
-    }
-
-
-    private void ReadyInactivate()
-    {
-
-        for (int i = 0; i < _particles.Length; i++)
-        {
-            var main = _particles[i].main;
-            main.loop = false;
-        }
-
-        if (_spriteRenderer != null)
-            _spriteRenderer.enabled = false;
-
-        if (_particles == null)
-        {
-            Inactivate();
-        }
-        else
-        {
-            int cnt = 0;
-            for (int i = 0; i < _particles.Length; i++)
+            _spriteRenderer = GetComponentInChildren<SpriteRenderer>(true);
+            if (_spriteRenderer != null)
             {
-                if (!_particles[i].isPlaying)
+                //_spriteRenderer.sortingLayerName = "FrontEffect";
+                //_spriteRenderer.sortingOrder = (int)-transform.position.y - 5;
+                _spriteRenderer.enabled = true;
+            }
+
+            _particles = _prefab.GetComponentsInChildren<ParticleSystem>(true);
+            //if (_particles != null)
+            //{
+            //    for (int i = 0; i < _particles.Length; i++)
+            //    {
+            //        var psRenderer = _particles[i].GetComponent<ParticleSystemRenderer>();
+            //        if(psRenderer != null)
+            //        {
+            //            psRenderer.sortingLayerName = "FrontEffect";
+            //            psRenderer.sortingOrder = (int)-transform.position.y + 10;
+            //        }
+            //    }
+            //}
+            gameObject.SetActive(true);
+
+        }
+
+        public void Inactivate()
+        {
+            gameObject.SetActive(false);
+            _startPos = Vector2.zero;
+            _arrivePos = Vector2.zero;
+            _nowTime = 0f;
+            _inactivateEvent?.Invoke(this);
+        }
+
+        public void CleanUp()
+        {
+            DestroyImmediate(_prefab);
+            _data = null;
+            _arrivedEvent = null;
+            _inactivateEvent = null;
+            _startPos = Vector2.zero;
+            _arrivePos = Vector2.zero;
+            _prefab = null;
+        }
+
+        private void Update()
+        {
+
+            _nowTime = CalculateTime(_nowTime);
+            transform.position = GetPosition(_startPos, _arrivePos, _nowTime);
+
+            if (_data.IsRotate) transform.eulerAngles = GetEuler(_startPos, _arrivePos, _nowTime);
+
+            //Debug.Log(transform.position + " " + _startPos + " " + _arrivePos + " " + _nowTime);
+
+            if (Vector2.Distance(transform.position, _arrivePos) < ARRIVE_DISTANCE)
+            {
+                if (!_isEffectActivate)
                 {
-                    cnt++;
+                    OnArriveEvent();
+                    EffectManager.Current.Activate(_data.ArriveEffectData, transform.position);
+                    _isEffectActivate = true;
                 }
             }
-            if (cnt == _particles.Length)
+
+            if (_isEffectActivate)
+            {
+                ReadyInactivate();
+            }
+
+        }
+
+        private float CalculateTime(float nowTime)
+        {
+            return nowTime += Time.deltaTime * _data.MovementSpeed;
+        }
+
+        private Vector3 GetEuler(Vector3 startPos, Vector3 arrivePos, float nowTime)
+        {
+            switch (_data.TypeBulletAction)
+            {
+                case BulletData.TYPE_BULLET_ACTION.Drop:
+                    startPos = new Vector2(arrivePos.x, arrivePos.y + 10f);
+                    break;
+                    //case BulletData.TYPE_BULLET_ACTION.Curve:
+                    //    //현재 위치에서 미래 위치를 예측해서 각도 찾기
+                    //    var futurePos = GetSlerp(startPos, arrivePos, CalculateTime(nowTime));
+                    //    startPos = transform.position;
+                    //    arrivePos = futurePos;
+                    //    break;
+            }
+
+            var direction = arrivePos - startPos;
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            return Vector3.forward * angle;
+        }
+
+        private Vector2 GetPosition(Vector3 startPos, Vector3 arrivePos, float nowTime)
+        {
+            switch (_data.TypeBulletAction)
+            {
+                case BulletData.TYPE_BULLET_ACTION.Curve:
+                case BulletData.TYPE_BULLET_ACTION.Move:
+                    return Vector2.MoveTowards(startPos, arrivePos, nowTime);
+                case BulletData.TYPE_BULLET_ACTION.Drop:
+                    var pos = new Vector2(arrivePos.x, arrivePos.y + 10f);
+                    return Vector2.MoveTowards(pos, arrivePos, nowTime);
+                case BulletData.TYPE_BULLET_ACTION.Direct:
+                    return arrivePos;
+                    //case BulletData.TYPE_BULLET_ACTION.Curve:
+                    //return GetSlerp(startPos, arrivePos, nowTime);
+
+            }
+            return Vector2.zero;
+        }
+
+        private Vector2 GetSlerp(Vector3 startPos, Vector3 arrivePos, float nowTime)
+        {
+            var center = (startPos + arrivePos) * 8f;
+            center -= Vector3.up;
+
+            var startRelPos = startPos - center;
+            var arriveRelPos = arrivePos - center;
+            var nowPos = Vector3.Slerp(startRelPos, arriveRelPos, nowTime);
+            nowPos += center;
+            return nowPos;
+        }
+
+
+        private void ReadyInactivate()
+        {
+
+            for (int i = 0; i < _particles.Length; i++)
+            {
+                var main = _particles[i].main;
+                main.loop = false;
+            }
+
+            if (_spriteRenderer != null)
+                _spriteRenderer.enabled = false;
+
+            if (_particles == null)
             {
                 Inactivate();
             }
+            else
+            {
+                int cnt = 0;
+                for (int i = 0; i < _particles.Length; i++)
+                {
+                    if (!_particles[i].isPlaying)
+                    {
+                        cnt++;
+                    }
+                }
+                if (cnt == _particles.Length)
+                {
+                    Inactivate();
+                }
+            }
         }
+
+        private void OnArriveEvent()
+        {
+            _arrivedEvent?.Invoke(this);
+        }
+
+
+
     }
-
-    private void OnArriveEvent()
-    {
-        _arrivedEvent?.Invoke(this);
-    }
-
-   
-
 }
--- Assets/Scripts/Utility/BulletManager/BulletData.cs
+++ Assets/Scripts/Utility/BulletManager/BulletData.cs
@@ -1,98 +1,102 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Storage;
-
-[CreateAssetMenu(fileName = "BulletData", menuName = "ScriptableObjects/BulletData")]
-public class BulletData : ScriptableObject //IComponentData
+namespace UtilityManager
 {
-    public enum TYPE_BULLET_ACTION { Move, Curve, Drop, Direct }
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Storage;
 
-    [SerializeField]
-    private GameObject _bulletPrefab;
+    [CreateAssetMenu(fileName = "BulletData", menuName = "ScriptableObjects/BulletData")]
+    public class BulletData : ScriptableObject //IComponentData
+    {
+        public enum TYPE_BULLET_ACTION { Move, Curve, Drop, Direct }
 
-    [SerializeField]
-    private string _bulletPrefabKey;
+        [SerializeField]
+        private GameObject _bulletPrefab;
 
-    [SerializeField]
-    private TYPE_BULLET_ACTION _typeBulletAction;
+        [SerializeField]
+        private string _bulletPrefabKey;
 
-    [SerializeField]
-    private EffectData _arriveEffectData;
+        [SerializeField]
+        private TYPE_BULLET_ACTION _typeBulletAction;
 
-    [SerializeField]
-    private string _arriveEffectDataKey;
+        [SerializeField]
+        private EffectData _arriveEffectData;
 
-    [SerializeField]
-    private float _movementSpeed = 1f;
+        [SerializeField]
+        private string _arriveEffectDataKey;
 
-    [SerializeField]
-    private bool _isRotate = false;
+        [SerializeField]
+        private float _movementSpeed = 1f;
+
+        [SerializeField]
+        private bool _isRotate = false;
 
 #if UNITY_EDITOR || UNITY_INCLUDE_TESTS
-    public static BulletData CreateTest()
-    {
-        return new BulletData();
-    }
-
-    public void SetData(TYPE_BULLET_ACTION typeBulletAction, float movementSpeed, bool isRotate)
-    {
-        _typeBulletAction = typeBulletAction;
-        _movementSpeed = movementSpeed;
-        _isRotate = isRotate;
-    }
-
-    private static Sprite _instanceSprite;
-
-    private BulletData()
-    {
-        var obj = new GameObject();
-        obj.name = "Data@Bullet";
-        var sprite = obj.AddComponent<SpriteRenderer>();
-
-
-        if (_instanceSprite == null)
+        public static BulletData CreateTest()
         {
-            Texture2D texture = new Texture2D(100, 100);
-
-            for (int y = 0; y < texture.height; y++)
-            {
-                for (int x = 0; x < texture.width; x++)
-                {
-                    texture.SetPixel(x, y, Color.white);
-                }
-            }
-            _instanceSprite = Sprite.Create(texture, new Rect(0, 0, 100, 100), Vector2.one * 0.5f);
+            return new BulletData();
         }
 
-        sprite.sprite = _instanceSprite;
-        _bulletPrefab = obj;
-    }
+        public void SetData(TYPE_BULLET_ACTION typeBulletAction, float movementSpeed, bool isRotate)
+        {
+            _typeBulletAction = typeBulletAction;
+            _movementSpeed = movementSpeed;
+            _isRotate = isRotate;
+        }
+
+        private static Sprite _instanceSprite;
+
+        private BulletData()
+        {
+            var obj = new GameObject();
+            obj.name = "Data@Bullet";
+            var sprite = obj.AddComponent<SpriteRenderer>();
+
+
+            if (_instanceSprite == null)
+            {
+                Texture2D texture = new Texture2D(100, 100);
+
+                for (int y = 0; y < texture.height; y++)
+                {
+                    for (int x = 0; x < texture.width; x++)
+                    {
+                        texture.SetPixel(x, y, Color.white);
+                    }
+                }
+                _instanceSprite = Sprite.Create(texture, new Rect(0, 0, 100, 100), Vector2.one * 0.5f);
+            }
+
+            sprite.sprite = _instanceSprite;
+            _bulletPrefab = obj;
+        }
 #endif
 
 
-    #region ##### Getter Setter #####
-    public GameObject prefab {
-        get
+        #region ##### Getter Setter #####
+        public GameObject prefab
         {
-            if (_bulletPrefab == null && !string.IsNullOrEmpty(_bulletPrefabKey))
-                _bulletPrefab = DataStorage.Instance.GetDataOrNull<GameObject>(_bulletPrefabKey, "Bullet", null);
-            return _bulletPrefab;
+            get
+            {
+                if (_bulletPrefab == null && !string.IsNullOrEmpty(_bulletPrefabKey))
+                    _bulletPrefab = DataStorage.Instance.GetDataOrNull<GameObject>(_bulletPrefabKey, "Bullet", null);
+                return _bulletPrefab;
+            }
         }
-    }
-    public EffectData ArriveEffectData
-    {
-        get
+        public EffectData ArriveEffectData
         {
-            if (_arriveEffectData == null && !string.IsNullOrEmpty(_arriveEffectDataKey))
-                _arriveEffectData = DataStorage.Instance.GetDataOrNull<EffectData>(_arriveEffectDataKey);
-            return _arriveEffectData;
+            get
+            {
+                if (_arriveEffectData == null && !string.IsNullOrEmpty(_arriveEffectDataKey))
+                    _arriveEffectData = DataStorage.Instance.GetDataOrNull<EffectData>(_arriveEffectDataKey);
+                return _arriveEffectData;
+            }
         }
+        public bool IsRotate => _isRotate;
+        public float MovementSpeed => _movementSpeed;
+        public TYPE_BULLET_ACTION TypeBulletAction => _typeBulletAction;
+
+        #endregion
+
     }
-    public bool IsRotate => _isRotate;
-    public float MovementSpeed => _movementSpeed;
-    public TYPE_BULLET_ACTION TypeBulletAction => _typeBulletAction;
-
-    #endregion
-
 }
