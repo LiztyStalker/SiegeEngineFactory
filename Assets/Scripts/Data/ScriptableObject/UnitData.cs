db9312adf0893c4ee5005df3f26ad8a897e4c34d
LiztyStalker <31952112+LiztyStalker@users.noreply.github.com>
2021-12-26 22:03:33 +0900

    FIX : UnitData Editor화 초안

--- Assets/Scripts/Data/ScriptableObject/UnitData.cs
+++ Assets/Scripts/Data/ScriptableObject/UnitData.cs
@@ -2,56 +2,81 @@ namespace SEF.Data
 {
     using UnityEngine;
 
+    [CreateAssetMenu(fileName = "UnitData", menuName = "ScriptableObjects/UnitData")]    
     public class UnitData : ScriptableObject
     {
         public enum TYPE_UNIT_GROUP { Thrower, Ram, Ballista, Catapult, MuzzleLoading, BreechLoading, RunOutCylinder, Missile}
         public enum TYPE_ATTACK_RANGE { Gun, Howitzer, Mortar, Melee}
 
+        [SerializeField]
         private string _key;
         public string Name => _key;
 
+        [SerializeField]
+        private string _spineModelKey;
+        public string SpineModelKey => _spineModelKey;
+
+        [SerializeField]
+        private string _spineSkinKey;
+        public string SpineSkinKey => _spineSkinKey;
+
+        [SerializeField]
         private TYPE_UNIT_GROUP _group;
         public TYPE_UNIT_GROUP Group => _group;
 
+        [SerializeField]
         private HealthData _healthValue;
         public HealthData HealthValue => _healthValue;
 
+        [SerializeField]
         private HealthData _increaseHealthValue;
         public HealthData IncreaseHealthValue => _increaseHealthValue;
 
+        [SerializeField]
         private float _increaseHealthRate;
         public float IncreaseHealthRate => _increaseHealthRate;
 
+        [SerializeField]
         private float _productTime;
         public float ProductTime => _productTime;
 
+        [SerializeField]
         private AttackData _attackValue;
         public AttackData AttackValue => _attackValue;
 
+        [SerializeField]
         private AttackData _increaseAttackValue;
         public AttackData IncreaseAttackValue => _increaseAttackValue;
 
+        [SerializeField]
         private float _increaseAttackRate;
         public float IncreaseAttackRate => _increaseAttackRate;
 
+        [SerializeField]
         private TYPE_ATTACK_RANGE _typeAttackRange;
         public TYPE_ATTACK_RANGE TypeAttackRange => _typeAttackRange;
 
+        [SerializeField]
         private int _attackPopulation;
         public int AttackPopulation => _attackPopulation;
 
+        [SerializeField]
         private int _attackCount;
         public int AttackCount => _attackCount;
 
+        [SerializeField]
         private float[] _attackDelay;
         public float[] AttackDelay => _attackDelay;
 
+        [SerializeField]
         private AssetData _startUpgradeAsset;
         public AssetData StartUpgradeAsset => _startUpgradeAsset;
 
+        [SerializeField]
         private AssetData _increaseUpgradeAssetValue;
         public AssetData IncreaseUpgradeAssetValue => _increaseUpgradeAssetValue;
 
+        [SerializeField]
         private float _increaseUpgradeAssetRate;
         public float IncreaseUpgradeAssetRate => _increaseUpgradeAssetRate;
 
@@ -61,9 +86,11 @@ namespace SEF.Data
         //private AssetData[] _conditionTechTreeValue;
         //public AssetData[] ConditionTechTreeValue => _conditionTechTreeValue;
 
+        [SerializeField]
         private AssetData _techTreeAsset;
         public AssetData TechTreeAsset => _techTreeAsset;
 
+        [SerializeField]
         private string[] _techTreeKeys;
         public string[] TechTreeKeys => _techTreeKeys;
 
@@ -80,6 +107,7 @@ namespace SEF.Data
         {
 
             _key = "Test";
+            _spineModelKey = "BowSoldier_SkeletonData";
             _group = TYPE_UNIT_GROUP.Thrower;
 
             _healthValue = HealthData.Create_Test();
