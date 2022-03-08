1e5251a850b180b2ff7ceb41478e26830d9a2651
LiztyStalker <31952112+LiztyStalker@users.noreply.github.com>
2022-03-08 15:51:20 +0900

    NEW : BulletDataGenerator 제작

--- Assets/Scripts/Utility/BulletManager/BulletData.cs
+++ Assets/Scripts/Utility/BulletManager/BulletData.cs
@@ -1,12 +1,12 @@
 namespace UtilityManager
 {
-    using System.Collections;
-    using System.Collections.Generic;
     using UnityEngine;
     using Storage;
+    using Utility.Data;
+    using SEF.Data;
 
     [CreateAssetMenu(fileName = "BulletData", menuName = "ScriptableObjects/BulletData")]
-    public class BulletData : ScriptableObject //IComponentData
+    public class BulletData : ScriptableObjectData
     {
         public enum TYPE_BULLET_ACTION { Move, Curve, Drop, Direct }
 
@@ -29,6 +29,9 @@ namespace UtilityManager
         private float _movementSpeed = 1f;
 
         [SerializeField]
+        private float _height = 0f;
+
+        [SerializeField]
         private bool _isRotate = false;
 
 #if UNITY_EDITOR || UNITY_INCLUDE_TESTS
@@ -42,6 +45,55 @@ namespace UtilityManager
             _typeBulletAction = typeBulletAction;
             _movementSpeed = movementSpeed;
             _isRotate = isRotate;
+        }
+
+        public override void SetData(string[] arr)
+        {
+            Key = arr[(int)BulletDataGenerator.TYPE_SHEET_COLUMNS.Key];
+
+            name = $"{typeof(UnitData).Name}_{Key}";
+
+            _typeBulletAction = (TYPE_BULLET_ACTION)System.Enum.Parse(typeof(TYPE_BULLET_ACTION), arr[(int)BulletDataGenerator.TYPE_SHEET_COLUMNS.TypeBulletAction]);
+            _movementSpeed = float.Parse(arr[(int)BulletDataGenerator.TYPE_SHEET_COLUMNS.MoveSpeed]);
+            _height = float.Parse(arr[(int)BulletDataGenerator.TYPE_SHEET_COLUMNS.Height]);
+            _isRotate = bool.Parse(arr[(int)BulletDataGenerator.TYPE_SHEET_COLUMNS.IsRotate]);
+
+            if (arr.Length > (int)BulletDataGenerator.TYPE_SHEET_COLUMNS.BulletSpriteKey)
+            {
+                _bulletPrefabKey = arr[(int)BulletDataGenerator.TYPE_SHEET_COLUMNS.BulletSpriteKey];
+                _arriveEffectDataKey = arr[(int)BulletDataGenerator.TYPE_SHEET_COLUMNS.BulletHitKey];
+            }
+        }
+
+        public override void AddData(string[] arr)
+        {
+            Debug.LogWarning("사용하지 않음");
+        }
+
+        public override bool HasDataArray()
+        {
+            Debug.LogWarning("사용하지 않음");
+            return false;
+        }
+
+        public override string[] GetData()
+        {
+            string[] arr = new string[System.Enum.GetValues(typeof(BulletDataGenerator.TYPE_SHEET_COLUMNS)).Length];
+
+            arr[(int)BulletDataGenerator.TYPE_SHEET_COLUMNS.TypeBulletAction] = _typeBulletAction.ToString();
+            arr[(int)BulletDataGenerator.TYPE_SHEET_COLUMNS.MoveSpeed] = _movementSpeed.ToString();
+            arr[(int)BulletDataGenerator.TYPE_SHEET_COLUMNS.Height] = _height.ToString();
+            arr[(int)BulletDataGenerator.TYPE_SHEET_COLUMNS.IsRotate] = _isRotate.ToString();
+            arr[(int)BulletDataGenerator.TYPE_SHEET_COLUMNS.BulletSpriteKey] = _bulletPrefabKey;
+            arr[(int)BulletDataGenerator.TYPE_SHEET_COLUMNS.BulletHitKey] = _arriveEffectDataKey;
+
+            return arr;
+        }
+
+        public override string[][] GetDataArray()
+        {
+            Debug.LogWarning("사용하지 않음");
+            return null;
         }
 
         private static Sprite _instanceSprite;
